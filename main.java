f5bdb5d76ff44c6aeafc728e75f5012a4c5ec2a0
amicha19 <158494961+amicha19@users.noreply.github.com>
2024-02-12 17:58:20 -0500

    Update main.java

--- main.java
+++ main.java
@@ -3,19 +3,22 @@
 // ----------------------------------------------------------
 // This program will convert Celsius to Fahrenheit
 // ----------------------------------------------------------
+
 public class Main {
   public static void main(String[] args) {
     
     // init
+    
     Scanner input = new Scanner(System.in);
     
     // program greeting
+    
     System.out.println(
       "\nConverts temperatures in Celsius to Fahrenheit\n");
 
     // prompt for input
     System.out.println("Enter temperature (in Celsius): ");
-    cel_temp = input.next();
+    int cel_temp = input.nextInt();
 
     // display results
     System.out.println(cel_temp + " Celsius = " + 
@@ -25,7 +28,7 @@ public class Main {
   }
 
   public static double convertToFahrenheit(int c) {
-    return (9/5 * c) + 32;
+    return (9.0/5.0 * c) + 32;
   }
   
 }
